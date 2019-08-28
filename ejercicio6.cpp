6aac77e60992b714151f6275d32d55575bcaeff8
davento <daniela.vento@utec.edu.pe>
2019-08-27 21:17:31 -0500

    Ejercicio completado en casa.

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -4,3 +4,23 @@
  Y encuentra el mayor valor: 30
 
  */
+
+#include <iostream>
+using namespace std;
+
+int findMax(int a,int b,int c){
+    int max=a;
+    if(max<b)
+        max=b;
+    if(max<c)
+        max=c;
+return max;
+}
+
+int main(){
+    int a, b, c;
+    cout<<"Ingrese tres números separados por un espacio entre cada uno: ";
+    cin>>a>>b>>c;
+    cout<<"\nEl mayor valor es: "<<findMax(a,b,c)<<"\n";
+return 0;
+}

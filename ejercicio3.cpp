8dfc44265774c17e88769d3ef40edc7064dc0d32
davento <daniela.vento@utec.edu.pe>
2019-08-27 19:11:34 -0500

    Ejercicio completado en casa.

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,12 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
 
 int mediana(vector<int> &lista);
 vector<int> leerConsola();
 
 vector<int> leerConsola() {
+    vector<int> v;
+
+    int num;
+
+    while(true){
+
+    	cin>>num;
+
+    	if (num==-1){
+
+    		break;
+
+    	}
+
+    	v.push_back(num);
+
+    }
+
+    return v;
     //Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:
 }
 
+vector<int> inSort(vector<int> &lista){
+int h, temp;
+for (int i=1;i<lista.size();i++){
+    h=i;
+    while(h >=0 && lista[h] < lista[h-1]){
+        temp=lista[h];
+        lista[h]=lista[h-1];
+        lista[h-1] = temp;
+        h=h-1;
+    }
+}
+return lista;
+}
+
 int mediana(vector<int> &lista) {
+    int size=lista.size(), med;
+    if (size%2==0)
+        med=(lista[size/2]+lista[size/2 - 1])/2;
+    else
+        med=lista[(size-1)/2];
     /*
     Si el vector es 3, 9, 11, 15
     Si el número de observaciones es par,
@@ -19,12 +60,13 @@ int mediana(vector<int> &lista) {
      Por ejemplo, en la muestra 3, 9, 11, 15, 16, la mediana es 11
      v[4/2] = v[2] = 11
      */
-    return null;
+    return med;
 }
 
 int main() {
     cout<<"Calcular la mediana de los numeros ingresados:\n\n";
     vector<int> leer_usuario = leerConsola();
+    inSort(leer_usuario);
     int mediana_elemento = mediana(leer_usuario);
     cout<<"La mediana es: "<<mediana_elemento<<"\n";
 }
